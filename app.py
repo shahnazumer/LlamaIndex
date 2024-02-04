6a22f478ede89de95ef76f42e545826743f69bf5
shahnazumer <shahnazumer@hotmail.com>
2024-02-05 04:37:37 +0500

    initial setup

---
+++ app.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+import os
+
+
+# Set Open API Key and Streamlit 
+st.set_page_config(page_title="Chat with the Beauty BFF, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+st.title("Chat with the Beauty BFF, powered by LlamaIndex 💬🦙")
+
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Hello Gorgeous! Feel free to ask anything!"}
+    ]
+
+
+# Load data and create index
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert Beauty assistant and your job is to answer about beauty tips questions. Assume that all questions are related to the beauty Keep your answers based on facts – do not hallucinate features."))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+    
+service_context = ServiceContext.from_defaults(embed_model="local")
+
+index = load_data()
+
+if "query_engine" not in st.session_state.keys(): # Initialize the query engine
+        st.session_state.query_engine= index.as_query_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.query_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
